8c8fabd8631fbc2ce338032da078074bd5e194ba
BrilliantBen <benoit.schrijnemakers@student.howest.be>
2015-06-10 15:56:58 +0200

    aded resize to squares, closes #27

--- upload.php
+++ upload.php
@@ -2,6 +2,11 @@
 
 var_dump($_FILES)
 
+namespace abeautifulsite;
+// use Exception;
+
+require 'abeautifulsite/SimpleImage.php';
+
 $file       = $_FILES['file']['tmp_name'];
 $fileName   = $_FILES['file']['name'];
 $fileType   = $_FILES['file']['type'];
@@ -16,7 +21,20 @@ $time = time();
 $newName = $rootName.$time.'.'.$extension;
 
 // temporarily save file
-$moved = move_uploaded_file($_FILES["file"]["tmp_name"], "assets/images/".$newName );
+$moved = move_uploaded_file($_FILES["file"]["tmp_name"], "uploads/large/".$newName );
+
+//crop and move
+ $img = new SimpleImage();
+
+    // Crop
+    $img->load("uploads/large/".$newName)->crop(0, 0, 640, 640)->save("uploads/large/".$newName);
+    $img->load("uploads/large/".$newName)->crop(0, 0, 440, 440)->save("uploads/medium/".$newName);
+    $img->load("uploads/large/".$newName)->crop(0, 0, 220, 220)->save("uploads/small/".$newName);
+
+
+
+
+
 if ($moved) $path = "uploads/".$newName;
 
 $body = $_POST['body'];
@@ -33,3 +51,10 @@ if ($moved) {
 
 header('Content-Type:application/json');
 echo json_encode($arrayToSend);
+
+
+
+
+
+
+
